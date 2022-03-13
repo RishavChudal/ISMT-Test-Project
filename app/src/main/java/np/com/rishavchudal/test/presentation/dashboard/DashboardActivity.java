4a0e0096aab0113ad693796837a97a381edd73a5
Rishav Chudal <rishavchudal29@gmail.com>
2022-03-13 11:17:14 +0545

    Added more fragments, nav graph and tied up the bottom navigation view with nav controller

--- app/src/main/java/np/com/rishavchudal/test/presentation/dashboard/DashboardActivity.java
+++ app/src/main/java/np/com/rishavchudal/test/presentation/dashboard/DashboardActivity.java
@@ -1,18 +1,43 @@
 package np.com.rishavchudal.test.presentation.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import np.com.rishavchudal.test.R;
 
 public class DashboardActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FragmentContainerView navHostContainerView;
+    private NavHostFragment navHostFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        initViews();
+        setUpBottomNavigationViewWithNavController();
+    }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        navHostContainerView = findViewById(R.id.nav_host_fragment);
+    }
+
+    private void setUpBottomNavigationViewWithNavController() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        }
     }
 
     @Override
--- app/src/main/java/np/com/rishavchudal/test/presentation/dashboard/HomeFragment.java
+++ app/src/main/java/np/com/rishavchudal/test/presentation/dashboard/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import np.com.rishavchudal.test.R;
 
@@ -16,51 +17,41 @@ import np.com.rishavchudal.test.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btnHomeFragment;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new HomeFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initWidgets(view);
+        initButtonAction();
+        return view;
+    }
+
+    private void initWidgets(View view) {
+        btnHomeFragment = view.findViewById(R.id.btn_home_fragment);
+    }
+
+    private void initButtonAction() {
+        btnHomeFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
